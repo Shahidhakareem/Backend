cb23eb9cca7b06ad4a146609924dddce32af2d71
Shahidhakareem <shahidhakareem134@gmail.com>
2025-11-20 09:05:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 // --- Middlewares ---
 
 const allowedOrigins = [
-  "https://frontend-two-mu-20.vercel.app/", // your deployed frontend
+  "https://frontend-git-main-shahidha-abdul-kareems-projects.vercel.app/login", // your deployed frontend
   "http://localhost:5173" // local frontend for development
 ];
 
